530d36aabd97c29cc2d234066e3c222e20a299ab
1329555958@qq.com <1329555958@qq.com>
2018-04-18 11:25:03 +0800

    console

--- service/console.js
+++ service/console.js
@@ -2,7 +2,7 @@ var _ = require('lodash');
 function log(){
     var params = [new Date()];
 
-    args.forEach(arg=>{
+    arguments.forEach(arg=>{
         params.push(_.isObject(arg) ? JSON.stringify(arg) : arg);
     });
     console.log(...params);
