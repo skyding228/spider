43fcdb202f825b0b3ae8a43ef4edffc871765edf
1329555958@qq.com <1329555958@qq.com>
2018-04-18 10:33:52 +0800

    zmodem version

--- public/javascripts/zmodem.js
+++ public/javascripts/zmodem.js
@@ -1,9 +1,10 @@
-(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.zmodem = f()}})(function(){var define,module,exports;return (function(){function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s}return e})()({1:[function(require,module,exports){
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.zmodem = f()}})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var zmodem;
-function zmodemAttach(term, ws, opts) {
+function zmodemAttach(ws, opts) {
     if (opts === void 0) { opts = {}; }
+    var term = this;
     var senderFunc = function (octets) { return ws.send(new Uint8Array(octets)); };
     var zsentry;
     function _shouldWrite() {
@@ -32,10 +33,9 @@ function zmodemAttach(term, ws, opts) {
     ws.binaryType = 'arraybuffer';
     ws.addEventListener('message', handleWSMessage);
 }
-exports.zmodemAttach = zmodemAttach;
 function apply(terminalConstructor) {
     zmodem = (typeof window === 'object') ? window.Zmodem : { Browser: null };
-    terminalConstructor.prototype.zmodemAttach = zmodemAttach.bind(this, this);
+    terminalConstructor.prototype.zmodemAttach = zmodemAttach;
     terminalConstructor.prototype.zmodemBrowser = zmodem.Browser;
 }
 exports.apply = apply;
@@ -44,3 +44,4 @@ exports.apply = apply;
 
 },{}]},{},[1])(1)
 });
+//# sourceMappingURL=zmodem.js.map
