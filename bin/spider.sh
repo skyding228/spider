94b19ada2d69922098f4822a1baafcfdf7b75be4
1329555958@qq.com <1329555958@qq.com>
2018-04-18 17:16:04 +0800

    docker

--- bin/spider.sh
+++ bin/spider.sh
@@ -5,11 +5,11 @@ HOSTNAME=`hostname`
 LOG_DIR=/opt/logs
 IP=`/sbin/ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v 172.17| grep -v inet6 | awk '{print $2}' | tr -d "addr:"`
 
-name=spider-web
+name=spider
 case "$1" in
     start)
         docker rm -f $name
-        docker run -d -p $PORT:3000 -e PORT=$PORT -e HOSTNAME=$HOSTNAME -e IP=$IP -e MASTER=$MASTER -v $LOG_DIR:/opt/logs:ro --name $name spider-web /opt/spider/run.sh
+        docker run -d -p $PORT:3000 -e PORT=$PORT -e HOSTNAME=$HOSTNAME -e IP=$IP -e MASTER=$MASTER -v $LOG_DIR:/opt/logs:ro --name $name spider /opt/spider/run.sh
         nohup docker logs -f $name > ${name}.log 2>&1 &
         tail -f ${name}.log
         ;;
