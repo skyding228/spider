928916edd09642ade80e20651d84a16f8306de0c
1329555958@qq.com <1329555958@qq.com>
2018-04-25 16:28:50 +0800

    tags

--- public/js/hosts.js
+++ public/js/hosts.js
@@ -3,12 +3,18 @@
  */
 require('app').register.controller('hostsController', function ($scope, $myhttp, $timeout, $rootScope) {
     $scope.hosts = [];
+    $scope.tags = [];
+    $scope.tagHost = '';
+    $scope.loading = false;
 
     function loadHosts() {
         $myhttp.get('/spider/hosts', function (data) {
             data = data || [];
             data.forEach(host=> {
-                host.url = removeLastSlash(host.url) + (  host.master ? '' : '/spiderweb-node?' + $rootScope.TOKEN_PARAM);
+                if (host.master) {
+                    host.masterUrl = host.url;
+                }
+                host.home = removeLastSlash(host.url) + '/spiderweb-node?' + $rootScope.TOKEN_PARAM;
             });
             $scope.hosts = data;
         });
@@ -22,5 +28,31 @@ require('app').register.controller('hostsController', function ($scope, $myhttp,
         return url;
     }
 
+
+    function loadTags(host) {
+        $scope.loading = true;
+        $scope.tagHost = host.name;
+        var url = removeLastSlash(host.url) + '/spider/getTag?' + $rootScope.TOKEN_PARAM;
+        $scope.tags = [];
+        $myhttp.get(url, function (data) {
+            $scope.loading = false;
+            data.sort(sortApps);
+            $scope.tags = data;
+        });
+    }
+
+    function sortApps(t1, t2) {
+        if (t1.app > t2.app) {
+            return 1;
+        }
+        if (t1.app === t2.app) {
+            return 0;
+        }
+        if (t1.app < t2.app) {
+            return -1;
+        }
+    }
+
+    $scope.loadTags = loadTags;
     loadHosts();
 });
