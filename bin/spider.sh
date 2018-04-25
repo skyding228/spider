80eb089b2e3b1a5a1473efaed63bccd9e128370f
1329555958@qq.com <1329555958@qq.com>
2018-04-25 16:50:48 +0800

    run shell

--- bin/spider.sh
+++ bin/spider.sh
@@ -4,14 +4,17 @@ PORT=3000
 HOSTNAME=`hostname`
 LOG_DIR=/opt/logs
 IP=`/sbin/ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v 172.17| grep -v inet6 | awk '{print $2}' | tr -d "addr:"`
-USERS_FILE=
+USERS_FILE=/opt/spider/users
 
 name=spider
-OPTIONS='-v '$USERS_FILE':/opt/spider/users -v /opt/applications:/opt/spider/applications:ro '
+OPTIONS=' -v /opt/applications:/opt/spider/applications:ro '
+if [ $USERS_FILE ];then
+  OPTIONS=$OPTIONS" -v "$USERS_FILE":/opt/spider/users "
+fi
 case "$1" in
     start)
         docker rm -f $name
-        docker run -d -p $PORT:3000 -e PORT=$PORT -e HOSTNAME=$HOSTNAME -e IP=$IP -e MASTER=$MASTER --hostname=$HOSTNAME -v $LOG_DIR:/opt/logs:ro $OPTIONS --name $name spider /opt/spider/run.sh
+        docker run -d -p $IP:$PORT:3000 -e PORT=$PORT -e HOSTNAME=$HOSTNAME -e IP=$IP -e MASTER=$MASTER --hostname=$HOSTNAME -v $LOG_DIR:/opt/logs:ro $OPTIONS --name $name spider /opt/spider/run.sh
         nohup docker logs -f $name > ${name}.log 2>&1 &
         tail -f ${name}.log
         ;;
