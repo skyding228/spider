dc1a82fca55f7d364053d5b478cbe2b65f3dbb93
1329555958@qq.com <1329555958@qq.com>
2018-04-25 18:25:04 +0800

    turn off debug

--- service/console.js
+++ service/console.js
@@ -1,12 +1,16 @@
 var _ = require('lodash');
-function log(){
+var debug = process.env.DEBUG;
+function log() {
+    if (!debug || 'false' === debug) {
+        return;
+    }
     var params = [new Date()];
-    for(var i=0;i<arguments.length;i++){
+    for (var i = 0; i < arguments.length; i++) {
         var arg = arguments[i];
         params.push(_.isObjectLike(arg) ? JSON.stringify(arg) : arg);
     }
     console.log(...params);
 }
 module.exports = {
-    log:log
+    log: log
 };
