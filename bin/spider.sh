42e686c1e3067bcf5bf434fd626371e96ba5f35d
1329555958@qq.com <1329555958@qq.com>
2018-04-25 15:21:30 +0800

    dockerfile

--- bin/spider.sh
+++ bin/spider.sh
@@ -11,7 +11,7 @@ OPTIONS='-v '$USERS_FILE':/opt/spider/users'
 case "$1" in
     start)
         docker rm -f $name
-        docker run -d -p $PORT:3000 -e PORT=$PORT -e HOSTNAME=$HOSTNAME -e IP=$IP -e MASTER=$MASTER -v $LOG_DIR:/opt/logs:ro $OPTIONS --name $name spider /opt/spider/run.sh
+        docker run -d -p $PORT:3000 -e PORT=$PORT -e HOSTNAME=$HOSTNAME -e IP=$IP -e MASTER=$MASTER --hostname=$HOSTNAME -v $LOG_DIR:/opt/logs:ro $OPTIONS --name $name spider /opt/spider/run.sh
         nohup docker logs -f $name > ${name}.log 2>&1 &
         tail -f ${name}.log
         ;;
